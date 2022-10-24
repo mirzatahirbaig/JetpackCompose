7190dfa78bd772a47042e1d6f977eb546108475d
Mirza Tahir Baig <tahir@mobizion.com>
2022-10-24 14:35:44 +0500

    Create Greet Composable
    Learn How to create Composable functions in jetpack compose

--- helloworld/app/src/main/java/com/mobizion/helloworld/MainActivity.kt
+++ helloworld/app/src/main/java/com/mobizion/helloworld/MainActivity.kt
@@ -3,11 +3,16 @@ package com.mobizion.helloworld
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mobizion.helloworld.ui.theme.HelloWorldTheme
@@ -17,12 +22,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HelloWorldTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greetings(message = "World")
                 }
             }
         }
@@ -30,14 +32,15 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greetings(message:String){
+    Text(text = "Hello $message")
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HelloWorldTheme {
-        Greeting("Android")
+        Greetings(message = "World")
     }
 }
